4d8e97600c534654ed38fc689e4158de0c0b5c4d
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-13 12:12:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//changing the card title
+let titl = document.getElementById("firtitl")
+titl.style.color= "red"
+
+let til = document.querySelectorAll(".card-title")
+til[0].style.color= "red"
+til[1].style.color= "green"
+til[2].style.color= "aqua"
+
+console.log(til)
+
+let kos = document.querySelector(".this")
+kos.style.color="black"
+kos.style.background="aqua"
+console.log(kos)
+
+console.log(document.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+console.log(document.getElementsByName("serc"))
+
+// document.querySelector(".this").style.background = "red"
+// console.log(document.getElementById())
